e1806d9d7f9d8047deddcbc337ef8a1da765513c
hikmatulloh <101179120+tuloAminov@users.noreply.github.com>
2022-12-01 19:52:34 +0500

    Add files via upload

---
+++ src/main/java/ru/students/startup/project/app/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package ru.students.startup.project.app.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
