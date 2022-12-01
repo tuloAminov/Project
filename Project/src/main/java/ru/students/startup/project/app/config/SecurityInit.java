c35e7b84caa3e526627c93b050bb53a5d8e409d5
hikmatulloh <101179120+tuloAminov@users.noreply.github.com>
2022-12-01 19:52:05 +0500

    Delete Project directory

--- Project/src/main/java/ru/students/startup/project/app/config/SecurityInit.java
+++
@@ -1,6 +0,0 @@
-package ru.students.startup.project.app.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-}
